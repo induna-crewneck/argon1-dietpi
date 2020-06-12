d5a1091cb14d560eb459ffd4eed70ade41d42333
induna-crewneck <66157637+induna-crewneck@users.noreply.github.com>
2020-06-12 11:35:52 +0200

    Update argon1diet-uninstall.sh

--- argon1diet-uninstall.sh
+++ argon1diet-uninstall.sh
@@ -2,10 +2,10 @@
 echo "-----------------------------------"
 echo "Argon One Uninstall Tool for DietPi"
 echo "-----------------------------------"
-timeout /t 1
+sleep 1
 echo "Sorry I'm not asking you to confirm. Couldn't get that to work."
 echo " ¯\_(ツ)_/¯"
-timeout /t 2
+sleep 2
 echo "But because I'm nice I'll give you 3 seconds to abort.
 timeout /3
 sudo systemctl stop argononed.service
@@ -29,17 +29,17 @@ sudo rm -r /root/argon1-dietpi
 sudo rm -r /etc/argon1
 echo "Removed all the Argon One shit."
 echo "Rebooting is highly recommended"
-timeout /t 1
+sleep 1
 echo "but I'm not your mom"
-timeout /t 1
+sleep 1
 echo "so do whatever you want"
-timeout /t 3
+sleep 3
 echo "No, you know what?"
-timeout /t 1
+sleep 1
 echo "You want me to do all the work, so I feel like I am entitled to have a say in this, too."
-timeout /t 3
+sleep 3
 echo "So I'll just reboot."
-timeout /t 1
+sleep 1
 echo "Deal with it."
-timeout /t 1
+sleep 1
 reboot
